b6a59254019f9416707bdb87bbfec5e98e2925a3
PuffyPuffin <PuffyPuffin@users.noreply.github.com>
2019-11-05 10:18:35 -0800

    Commented the creation of the fish_data_cat category

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -1,7 +1,11 @@
-fish_data = read.csv("data_raw/Gaeta_etal_CLC_data.csv")
+fish_data <-read.csv("data_raw/Gaeta_etal_CLC_data.csv")
 
 install.packages("dplyr")
 library(dplyr)
 
-fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+# This code adds a categoical size column sorted by big and small,
+# where any length bigger than 300 is considered big and anything 
+# smaller is considered small
+
+fish_data_cat <- fish_data %>%
+  mutate(length_cat <- ifelse(length > 300, "big", "small"))
