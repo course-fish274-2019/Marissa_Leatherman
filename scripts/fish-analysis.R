7b41152a3acb1d132181eb866c2912d3289e1414
PuffyPuffin <PuffyPuffin@users.noreply.github.com>
2019-11-07 09:50:11 -0800

    Added scatterplot of length vs scalelength with color based on lake ID

--- scripts/fish-analysis.R
+++ scripts/fish-analysis.R
@@ -1,6 +1,7 @@
 fish_data <-read.csv("data_raw/Gaeta_etal_CLC_data_1.csv")
 
 install.packages("dplyr")
+install.packages('tidyverse')
 library(dplyr)
 
 # This code adds a categoical size column sorted by big and small,
@@ -11,3 +12,9 @@ fish_data_cat <- fish_data %>%
   mutate(length_cat <- ifelse(length > 300, "big", "small"))
 
 fish_data_cat_new <- filter(fish_data_cat, scalelength > 1)
+
+library(ggplot2)
+
+ggplot(data=fish_data_cat_new, mapping=(aes(x = length, y = scalelength, color = lakeid))) +
+         geom_point()+
+         labs(x = 'Body Length', y = 'Scale Length')
